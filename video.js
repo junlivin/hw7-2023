1931a5f1f76c55c128a40c3b089bad058944611a
junlivin <143903583+junlivin@users.noreply.github.com>
2023-11-19 14:53:22 -0500

    Add files via upload

---
+++ video.js
@@ -0,0 +1,72 @@
+var video;
+window.addEventListener("load", function() {
+	console.log("Good job opening the window");
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	video.loop = false;
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").textContent = "100%";
+});
+
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	if (video.playbackRate >= 0.2){
+		video.playbackRate = video.playbackRate - 0.1;
+	}
+	console.log("video speed is " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	if (video.playbackRate >= 2){
+		video.playbackRate = 2;
+	} else{
+		video.playbackRate = video.playbackRate + 0.1;
+	}
+	console.log("video speed is " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime < video.duration){
+		video.currentTime += 10;
+	} else if (video.currentTime >= video.duration){
+		video.currentTime = 0;
+	}
+	console.log(video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (this.textContent === "Mute"){
+		video.volume = 0;
+		this.textContent = "Unmute";
+		document.querySelector("#volume").textContent = '0%';
+		console.log(video.volume);
+	} else{
+		video.volume = document.querySelector("#slider").value/100;
+		this.textContent = "Mute";
+		document.querySelector("#volume").textContent = document.querySelector("#slider").value + '%';
+		console.log(video.volume);
+	}
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = this.value/100;
+	console.log(video.volume);
+	document.querySelector("#volume").textContent = this.value + '%';
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.setAttribute('class', 'oldSchool');
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.setAttribute('class', 'video');
+});
